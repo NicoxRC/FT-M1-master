0113ae76901f5ffba6bf32b07053cf13e50f0ba6
NicoxRC <nicorojas005@gmail.com>
2022-08-02 18:43:01 -0500

    first homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var str = num.toString();
+  var result = 0;
+  var expo = 0;
+  for (var i = str.length - 1; i >= 0; i--) {
+    if (str[i] == "1") {
+      result += Math.pow(2, expo);
+    }
+    expo++;
+  }
+  return result;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  var bin = [];
+  while (num >= 1) {
+    bin.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return bin.reverse().toString().replace(/,/g, "");
 }
 
 
