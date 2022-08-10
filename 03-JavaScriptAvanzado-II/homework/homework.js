52bcbd01721dd14b633d17b99077b450d13172aa
NicoxRC <nicorojas005@gmail.com>
2022-08-09 21:20:20 -0500

    homework 03

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -19,6 +19,11 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+
+  let counter = 1;
+  return function () {
+    return counter++;
+  }
 }
 
 function cacheFunction(cb) {
@@ -41,6 +46,13 @@ function cacheFunction(cb) {
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
+  let cache = {};
+  return function (arg) {
+    if (!cache.hasOwnProperty(arg)) {
+      cache[arg] = cb(arg);
+    }
+    return cache[arg];
+  }
 }
 
 // Bind
@@ -67,8 +79,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
@@ -80,9 +92,9 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind('','*','*');
+let textoGuiones = crearCadena.bind('','-','-');
+let textoUnderscore = crearCadena.bind('','_','_');
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
